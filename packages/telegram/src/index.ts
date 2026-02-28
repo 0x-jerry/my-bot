c6fe77020129658db49f8b0da83800f835afb479
Jerry <x.jerry.wang@gmail.com>
2026-02-28 11:49:06 +0800

    setup proxy

--- packages/telegram/src/index.ts
+++ packages/telegram/src/index.ts
@@ -1,6 +1,14 @@
+import "dotenv/config";
+import 'global-agent/bootstrap'
+import { setGlobalDispatcher, ProxyAgent } from 'undici'
 import { Bot } from "gramio";
 
-const bot = new Bot(process.env.BOT_TOKEN as string)
+if (process.env.GLOBAL_AGENT_HTTP_PROXY) {
+  const proxyAgent = new ProxyAgent(process.env.GLOBAL_AGENT_HTTP_PROXY)
+  setGlobalDispatcher(proxyAgent)
+}
+
+const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN as string)
     .command("start", (context) => context.send("Hello! I am your new GramIO bot!"))
     .onStart(({ info }) => console.log(`✨ Bot ${info.username} was started!`));
 
