791941465c09259f229ba56ecb2bcecce5ed0e37
Jerry <x.jerry.wang@gmail.com>
2026-03-12 06:37:05 +0000

    chore(agent): update config schema

--- packages/agent/src/config/types.ts
+++ packages/agent/src/config/types.ts
@@ -1,5 +1,3 @@
-import type { ToolSet } from "../toolset/types";
-
 export namespace Config {
   export interface Root {
     /**
@@ -80,5 +78,52 @@ export namespace Config {
      * Max number of messages to keep in history.
      */
     maxHistoryMessages?: number;
+  }
+}
+
+export namespace ToolSet {
+  export type All = Mcp | Skill | Memory | Shell;
+
+  export interface Mcp extends McpConfig {
+    type: "mcp";
+
+    /**
+     * Root level mcp key
+     */
+    name?: string;
+  }
+
+  export interface McpConfig {
+    command?: string;
+    args?: string[];
+    env?: Record<string, string>;
+    envFile?: string[];
+
+    remoteUrl?: string;
+    remoteType?: "http" | "sse";
+
+    /**
+     * Filter tools by tool name.
+     */
+    filterTools?: string[];
+  }
+  export interface Skill {
+    type: "skill";
+  }
+
+  export interface Memory {
+    type: "memory";
+    /**
+     * The path to the memory file.
+     */
+    file?: string;
+    /**
+     * The URL of the memory server.
+     */
+    remoteUrl?: string;
+  }
+
+  export interface Shell {
+    type: "shell";
   }
 }
--- packages/agent/src/toolset/types.ts
+++ packages/agent/src/toolset/types.ts
@@ -1,52 +1,6 @@
 import type { Tool } from "ai";
 import type { IDisposable } from "../types";
-
-export namespace ToolSet {
-  export type All = Mcp | Skill | Memory | Shell;
-
-  export interface Mcp extends McpConfig {
-    type: "mcp";
-
-    /**
-     * Root level mcp key
-     */
-    name?: string
-  }
-
-  export interface McpConfig {
-    command?: string;
-    args?: string[];
-    env?: Record<string, string>;
-    envFile?: string[];
-
-    remoteUrl?: string;
-    remoteType?: "http" | "sse";
-
-    /**
-     * Filter tools by tool name.
-     */
-    filterTools?: string[];
-  }
-  export interface Skill {
-    type: "skill";
-  }
-
-  export interface Memory {
-    type: "memory";
-    /**
-     * The path to the memory file.
-     */
-    file?: string;
-    /**
-     * The URL of the memory server.
-     */
-    remoteUrl?: string;
-  }
-
-  export interface Shell {
-    type: "shell";
-  }
-}
+export type { ToolSet } from "../config/types";
 
 export interface LoadedToolset extends Partial<IDisposable> {
   toolset: Record<string, Tool>;
