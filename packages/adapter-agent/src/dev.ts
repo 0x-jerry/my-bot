4e1d90bb220993afd77ba09012e05d8294652dd5
Jerry <x.jerry.wang@gmail.com>
2026-03-14 13:14:30 +0800

    chore: evaluate ai agent

--- packages/adapter-agent/src/dev.ts
+++ packages/adapter-agent/src/dev.ts
@@ -1,25 +1,60 @@
+import pc from "picocolors";
 import { MyAgentAdapter } from "./myagent";
 
-const cagent = new MyAgentAdapter({
-  baseUrl: "http://localhost:9756",
+const agent = new MyAgentAdapter({
+  baseUrl: "http://localhost:10080",
 });
 
-const agents = await cagent.agents();
-console.log(agents);
+const session = await agent.sessions.create();
 
-const sessions = await cagent.sessions.list();
-console.log(sessions);
+const sessionId = session.id;
 
-const agentId = agents.at(0)!.id;
-const sessionId = sessions.at(0)!.id;
+console.log("Session created with ID:", sessionId);
 
-const data = await cagent.sessions.get(sessionId);
-console.log(data);
+await oneTurn("删除所有任务");
 
-const msgs = await cagent.messages.getAll(sessionId);
+await oneTurn("明天早上8点告诉过当天的天气情况");
 
-console.log(msgs);
+async function oneTurn(msg: string) {
+  const resp = agent.messages.send(sessionId, msg);
 
-// for await (const chunk of cagent.messages.send(sessionId, "hello")) {
-//   console.log(chunk)
-// }
+  for await (const chunk of resp) {
+    switch (chunk.type) {
+      case "reasoning-start":
+        console.log("Thinking:");
+        break;
+      case "reasoning-delta":
+        process.stdout.write(pc.dim(chunk.delta));
+        break;
+      case "reasoning-end":
+        process.stdout.write("\n");
+        break;
+      case "text-start":
+        console.log("Response:");
+        break;
+      case "text-delta":
+        process.stdout.write(chunk.delta);
+        break;
+      case "text-end":
+        process.stdout.write("\n");
+        break;
+
+      case "error":
+        console.error("Error:", chunk.errorText);
+        break;
+      case "tool-input-start":
+        console.log("Call tool:", pc.cyan(chunk.toolName), " ");
+        break;
+      case "tool-input-delta":
+        process.stdout.write(pc.cyan(chunk.inputTextDelta));
+        break;
+
+      case "tool-output-available":
+        console.log("Tool output:", chunk.output);
+        break;
+
+      default:
+        break;
+    }
+  }
+}
