abe455babf9d36fa2b32f0c1e8f7fa90f873c58e
Jerry <x.jerry.wang@gmail.com>
2026-04-15 07:32:02 +0000

    chore: fix typo

--- packages/agent/docker/build.sh
+++ packages/agent/docker/build.sh
@@ -2,6 +2,6 @@ set -e
 
 cd "$(dirname "$0")"
 
-BASE_IMAGE_NAME="${BASE_IMAGE_NAME:-"my-agent:base-v0"}"
+BASE_IMAGE_NAME="${BASE_IMAGE_NAME:-"my-agent:base"}"
 
 docker build --rm -f "Dockerfile.dev" -t "${BASE_IMAGE_NAME}" .
--- packages/agent/scripts/build-docker.sh
+++ packages/agent/scripts/build-docker.sh
@@ -4,9 +4,9 @@ bun run g:config-schema
 bun run db:generate
 bun run build
 
-BASE_IAMGE_NAME="my-agent:base-v1"
+BASE_IMAGE_NAME="my-agent:base-v1"
 
 # build base image
 sh docker/build.sh
 
-docker build --build-arg BASE_IMAGE_NAME="${BASE_IAMGE_NAME}" --rm -f Dockerfile -t my-agent:latest .
+docker build --build-arg BASE_IMAGE_NAME="${BASE_IMAGE_NAME}" --rm -f Dockerfile -t my-agent:latest .
